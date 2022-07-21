518c455774b8da7f0ede14bd6e24a04a83205e9f
Jaylen Garner <jaylen.garner@outlook.com>
2022-07-21 18:47:02 -0400

    Get all spots feature

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const apiRouter = require('./api');
+const spotRouter = require('./spots')
 
 router.use('/api', apiRouter);
+router.use('/spots', spotRouter)
 
 router.get("/api/csrf/restore", (req, res) => {
   const csrfToken = req.csrfToken();
